698772a18d231d119afd0b82c379346cf0fa2d26
CamPhu23 <49907658+CamPhu23@users.noreply.github.com>
2022-06-13 00:06:25 +0700

    admin login and statistics api

--- src/repositories/client-repository.ts
+++ src/repositories/client-repository.ts
@@ -3,7 +3,11 @@ import { BaseRepository } from "./base-repository";
 
 export class ClientRepository extends BaseRepository {
   async getClientByUsername(username: string): Promise<Client | null> {
-    const client = await ClientModel.findOne({username, isDelete: false});
+    const client = await ClientModel.findOne({ username, isDelete: false });
     return client ? Client.fromData(client) : null;
   };
+
+  async countAll(): Promise<Number | 0> {
+    return await ClientModel.countDocuments({});
+  }
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,22 @@
 import { ProductService } from './product-services';
 import { AuthService } from './auth-service';
 import { PaymentService } from './payment-service';
+import { AdminAuthService } from './admin/admin-auth-service';
+import { AdminAccountService } from './admin/admin-account-service';
+import { AdminStatisticsService } from './admin/admin-statistics-service';
 
 const productService = new ProductService();
 const authService = new AuthService();
 const paymentService = new PaymentService();
+const adminAuthService = new AdminAuthService();
+const adminAccountService = new AdminAccountService();
+const adminStatisticController = new AdminStatisticsService();
 
 export {
   productService,
   authService,
-  paymentService
+  paymentService,
+  adminAuthService,
+  adminAccountService,
+  adminStatisticController,
 };
